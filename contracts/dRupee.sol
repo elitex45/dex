6f658d5fc2c7cf1905afa92a4a90e8a9de75d60e
sp756897 <akshaysp2090@gmail.com>
2022-04-14 01:05:55 +0530

    first

---
+++ contracts/dRupee.sol
@@ -0,0 +1,12 @@
+pragma solidity >=0.8.0 <0.9.0;
+
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+contract drupee is ERC20 {
+  constructor() ERC20("drupee", "DRU") {
+  }
+
+  function mint_drupee(address dex_address) public{
+    _mint(dex_address,10000000*(10**18));
+  }
+}
