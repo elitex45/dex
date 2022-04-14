a9d20533cbfeaeb6ad50a54200a468b7baa634e6
thespes <gurugkgurukiran@gmail.com>
2022-04-14 06:51:59 +0530

    changed default selector in topnav

--- frontend/src/components/Topnavmenu.js
+++ frontend/src/components/Topnavmenu.js
@@ -11,7 +11,7 @@ class Topnavmenu extends React.Component {
     render() {
         return (
             <div>
-                <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
+                <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['swap']}>
                     <Menu.Item key='swap'>
                         <Link to="/">SWAP</Link>
                     </Menu.Item>
